bfe9a223ad5d44b4e772d42d4ef1aaf87ced3179
vol4tim <vol4tim@gmail.com>
2017-03-23 16:29:36 +0400

    init

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  daoAddress: '0x23e70c0462885766b150cdbf9402bff35dddeea7',
+  // complierAddress: '0xd8215991997bf965ba4da769087a6cfd61c01e16',
+  // accountAddress: '0x7453C2418d6b3A475A750022cCd01f378d60Fa95'
+}
---
+++ src/utils.js
@@ -0,0 +1,71 @@
+import Web3 from "web3";
+import _ from "lodash";
+import abi from "./abi";
+
+export const web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://parity:8545'));
+
+export const getProgramms = function(coreAddress) {
+  const programms = [];
+  const core = web3.eth.contract(abi.Core).at(coreAddress);
+  let next = core.first();
+  if (next == '0x0000000000000000000000000000000000000000') {
+    next = false;
+  }
+  while (next) {
+    const type = core.abiOf(next);
+    if (type == 'registry') {
+      let name = core.getName(next);
+      programms.push({
+        address: next,
+        name
+      });
+    }
+    next = core.next(next);
+    if (next == '0x0000000000000000000000000000000000000000') {
+      next = false;
+    }
+  }
+  return programms;
+}
+
+export const getTokens = function(coreAddress) {
+  const tokens = [];
+  const core = web3.eth.contract(abi.Core).at(coreAddress);
+  let next = core.first();
+  if (next == '0x0000000000000000000000000000000000000000') {
+    next = false;
+  }
+  while (next) {
+    let name = core.getName(next);
+    tokens.push({
+      address: next,
+      name
+    });
+    next = core.next(next);
+    if (next == '0x0000000000000000000000000000000000000000') {
+      next = false;
+    }
+  }
+  return tokens;
+}
+
+export const getBalance = function(tokenAddress, complierAddress) {
+  const token = web3.eth.contract(abi.Token).at(tokenAddress);
+  let balance = token.balanceOf(complierAddress);
+  return _.toNumber(balance);
+}
+
+// export const burn = function(tokenAddress, amount, complierAddress, accountAddress) {
+//   const complier = web3.eth.contract(abi.Complier).at(complierAddress);
+//   const tx = complier.burn(tokenAddress, amount, {from: accountAddress, gas: 100000});
+//   return tx;
+// }
+
+export const getTransactionCount = function(address) {
+  return web3.eth.getTransactionCount(address);
+}
+
+export const raw = function(code) {
+  return web3.eth.sendRawTransaction('0x'+code.replace(/^0x/, ''));
+}
