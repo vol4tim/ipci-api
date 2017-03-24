bfd537054c7fef6690d61ffd5250997b827c34fe
vol4tim <vol4tim@gmail.com>
2017-03-24 16:54:09 +0400

    promises

--- src/index.js
+++ src/index.js
@@ -17,45 +17,77 @@ app.get('/', (req, res) => {
 
 // http://localhost:3001/programms
 app.get('/programms', (req, res) => {
-  const programms = utils.getProgramms(DAO);
-  res.status(200).send({
-    programms
-  })
+  utils.getProgramms(DAO)
+    .then((programms) => {
+      res.status(200).send({
+        programms
+      })
+    })
+    .catch((e) => {
+      res.status(500).send({
+        error: e.toString()
+      })
+    });
 })
 
 // http://localhost:3001/tokens/0x9ee8b04a4a5e301eabdb791fb6ed4e7d815cf90d
 app.get('/tokens/:core', (req, res) => {
-  const tokens = utils.getTokens(req.params.core);
-  res.status(200).send({
-    tokens
-  })
+  utils.getTokens(req.params.core)
+    .then((tokens) => {
+      res.status(200).send({
+        tokens
+      })
+    })
+    .catch((e) => {
+      res.status(500).send({
+        error: e.toString()
+      })
+    });
 })
 
 // http://localhost:3001/balance/0x1c0cd50ae66352289181865cfa839c2bc8b7e9b3/0xd8215991997bf965ba4da769087a6cfd61c01e16
 app.get('/balance/:token/:complier', (req, res) => {
-  const balance = utils.getBalance(req.params.token, req.params.complier);
-  res.status(200);
-  res.send({
-    balance: _.toString(balance)
-  })
+  utils.getBalance(req.params.token, req.params.complier)
+    .then((balance) => {
+      res.status(200).send({
+        balance: _.toString(balance)
+      })
+    })
+    .catch((e) => {
+      res.status(500).send({
+        error: e.toString()
+      })
+    });
 })
 
 // http://localhost:3000/count/0x7453C2418d6b3A475A750022cCd01f378d60Fa95
 app.get('/count/:address', (req, res) => {
-  const count = utils.getTransactionCount(req.params.address);
-  res.status(200);
-  res.send({
-    count: count
-  })
+  utils.getTransactionCount(req.params.address)
+    .then((count) => {
+      res.status(200).send({
+        count: count
+      })
+    })
+    .catch((e) => {
+      res.status(500).send({
+        error: e.toString()
+      })
+    });
 })
 
 // http://localhost:3001/raw/asdasdasdasdasdasdasd
 app.get('/raw/:code', (req, res) => {
-  const tx = utils.raw(req.params.code);
-  res.status(200);
-  res.send({
-    tx: tx
-  })
+  utils.raw(req.params.code)
+    .then((tx) => {
+      res.status(200).send({
+        tx: tx
+      })
+    })
+  .catch((e) => {
+    res.status(500).send({
+      error: e.toString()
+    })
+  });
 })
 
 app.listen(3000)
--- src/utils.js
+++ src/utils.js
@@ -7,60 +7,84 @@ export const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(PARITY));
 
 export const getProgramms = function(coreAddress) {
-  const programms = [];
-  const core = web3.eth.contract(abi.Core).at(coreAddress);
-  let next = core.first();
-  if (next == '0x0000000000000000000000000000000000000000') {
-    next = false;
-  }
-  while (next) {
-    const type = core.abiOf(next);
-    if (type == 'registry') {
-      let name = core.getName(next);
-      programms.push({
-        address: next,
-        name
-      });
-    }
-    next = core.next(next);
+  return new Promise((resolve, reject) => {
+    const programms = [];
+    const core = web3.eth.contract(abi.Core).at(coreAddress);
+    let next = core.first();
     if (next == '0x0000000000000000000000000000000000000000') {
       next = false;
     }
-  }
-  return programms;
+    while (next) {
+      const type = core.abiOf(next);
+      if (type == 'registry') {
+        let name = core.getName(next);
+        programms.push({
+          address: next,
+          name
+        });
+      }
+      next = core.next(next);
+      if (next == '0x0000000000000000000000000000000000000000') {
+        next = false;
+      }
+    }
+    resolve(programms);
+  });
 }
 
 export const getTokens = function(coreAddress) {
-  const tokens = [];
-  const core = web3.eth.contract(abi.Core).at(coreAddress);
-  let next = core.first();
-  if (next == '0x0000000000000000000000000000000000000000') {
-    next = false;
-  }
-  while (next) {
-    let name = core.getName(next);
-    tokens.push({
-      address: next,
-      name
-    });
-    next = core.next(next);
+  return new Promise((resolve, reject) => {
+    const tokens = [];
+    const core = web3.eth.contract(abi.Core).at(coreAddress);
+    let next = core.first();
     if (next == '0x0000000000000000000000000000000000000000') {
       next = false;
     }
-  }
-  return tokens;
+    while (next) {
+      let name = core.getName(next);
+      tokens.push({
+        address: next,
+        name
+      });
+      next = core.next(next);
+      if (next == '0x0000000000000000000000000000000000000000') {
+        next = false;
+      }
+    }
+    resolve(tokens);
+  });
 }
 
 export const getBalance = function(tokenAddress, complierAddress) {
-  const token = web3.eth.contract(abi.Token).at(tokenAddress);
-  let balance = token.balanceOf(complierAddress);
-  return _.toNumber(balance);
+  return new Promise((resolve, reject) => {
+    const token = web3.eth.contract(abi.Token).at(tokenAddress);
+      token.balanceOf(complierAddress, (e, r) => {
+      if (e) {
+        reject(e);
+      }
+      resolve(r);
+    });
+  });
 }
 
 export const getTransactionCount = function(address) {
-  return web3.eth.getTransactionCount(address);
+  return new Promise((resolve, reject) => {
+    web3.eth.getTransactionCount(address, (e, r) => {
+      if (e) {
+        reject(e);
+      }
+      resolve(r);
+    });
+  });
 }
 
 export const raw = function(code) {
-  return web3.eth.sendRawTransaction('0x'+code.replace(/^0x/, ''));
+  return new Promise((resolve, reject) => {
+    web3.eth.sendRawTransaction('0x'+code.replace(/^0x/, ''), (e, r) => {
+      if (e) {
+        reject(e);
+      }
+      resolve(r);
+    });
+  });
 }
