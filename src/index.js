407ca5bb61de5c2ac1592fe3ef1ad2bf6e65ae3c
vol4tim <vol4tim@gmail.com>
2017-04-06 15:16:52 +0400

    getTimestamp, valid token

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   })
 })
 
-// http://localhost:3001/programms
+// http://localhost:3000/programms
 app.get('/programms', (req, res) => {
   utils.getProgramms(DAO)
     .then((programms) => {
@@ -30,7 +30,7 @@ app.get('/programms', (req, res) => {
     });
 })
 
-// http://localhost:3001/tokens/0x9ee8b04a4a5e301eabdb791fb6ed4e7d815cf90d
+// http://localhost:3000/tokens/0x9ee8b04a4a5e301eabdb791fb6ed4e7d815cf90d
 app.get('/tokens/:core', (req, res) => {
   utils.getTokens(req.params.core)
     .then((tokens) => {
@@ -45,7 +45,7 @@ app.get('/tokens/:core', (req, res) => {
     });
 })
 
-// http://localhost:3001/balance/0x1c0cd50ae66352289181865cfa839c2bc8b7e9b3/0xd8215991997bf965ba4da769087a6cfd61c01e16
+// http://localhost:3000/balance/0x1c0cd50ae66352289181865cfa839c2bc8b7e9b3/0xd8215991997bf965ba4da769087a6cfd61c01e16
 app.get('/balance/:token/:complier', (req, res) => {
   utils.getBalance(req.params.token, req.params.complier)
     .then((balance) => {
@@ -75,7 +75,82 @@ app.get('/count/:address', (req, res) => {
     });
 })
 
-// http://localhost:3001/raw/asdasdasdasdasdasdasd
+// http://localhost:3000/check/0x7453C2418d6b3A475A750022cCd01f378d60Fa95
+app.get('/check/:hash', (req, res) => {
+  utils.isTransactionComplete(req.params.hash)
+    .then((result) => {
+      res.status(200).send({
+        result: result
+      })
+    })
+    .catch((e) => {
+      res.status(500).send({
+        error: e.toString()
+      })
+    });
+})
+
+// http://localhost:3000/period/0x7453C2418d6b3A475A750022cCd01f378d60Fa95
+app.get('/period/:token', (req, res) => {
+  utils.getTimestamp(req.params.token)
+    .then((result) => {
+      result = _.toNumber(result);
+      const current = Math.round(new Date().getTime()/1000);
+      if (result > 0 && result <= current) {
+        res.status(200).send({
+          error: 'End period',
+          result: result
+        })
+      } else {
+        res.status(200).send({
+          result: result
+        })
+      }
+    })
+    .catch((e) => {
+      res.status(500).send({
+        error: e.toString()
+      })
+    });
+})
+
+// http://localhost:3000/valid/0x1c0cd50ae66352289181865cfa839c2bc8b7e9b3/0xd8215991997bf965ba4da769087a6cfd61c01e16/10
+app.get('/valid/:token/:complier/:amount', (req, res) => {
+  utils.getBalance(req.params.token, req.params.complier)
+    .then((balance) => {
+      balance = _.toNumber(balance)
+      if (balance < req.params.amount) {
+        res.status(500).send({
+          error: 'Amount deficiency'
+        })
+        return false;
+      } else {
+        return utils.getTimestamp(req.params.token)
+      }
+    })
+    .then((result) => {
+      if (result !== false) {
+        result = _.toNumber(result);
+        const current = Math.round(new Date().getTime()/1000);
+        if (result > 0 && result <= current) {
+          res.status(200).send({
+            error: 'End period',
+          })
+        } else {
+          res.status(200).send({
+            result: 'ok'
+          })
+        }
+      }
+    })
+    .catch((e) => {
+      res.status(500).send({
+        error: e.toString()
+      })
+    });
+})
+
+// http://localhost:3000/raw/asdasdasdasdasdasdasd
 app.get('/raw/:code', (req, res) => {
   utils.raw(req.params.code)
     .then((tx) => {
--- src/utils.js
+++ src/utils.js
@@ -9,25 +9,29 @@ web3.setProvider(new web3.providers.HttpProvider(PARITY));
 export const getProgramms = function(coreAddress) {
   return new Promise((resolve, reject) => {
     const programms = [];
-    const core = web3.eth.contract(abi.Core).at(coreAddress);
-    let next = core.first();
-    if (next == '0x0000000000000000000000000000000000000000') {
-      next = false;
-    }
-    while (next) {
-      const type = core.abiOf(next);
-      if (type == 'registry') {
-        let name = core.getName(next);
-        programms.push({
-          address: next,
-          name
-        });
-      }
-      next = core.next(next);
-      if (next == '0x0000000000000000000000000000000000000000') {
-        next = false;
-      }
-    }
+    programms.push({
+      address: '0x9ee8b04a4a5e301eabdb791fb6ed4e7d815cf90d',
+      name: 'Integrated Program for Climate Initiatives'
+    });
+    // const core = web3.eth.contract(abi.Core).at(coreAddress);
+    // let next = core.first();
+    // if (next == '0x0000000000000000000000000000000000000000') {
+    //   next = false;
+    // }
+    // while (next) {
+    //   const type = core.abiOf(next);
+    //   if (type == 'registry') {
+    //     let name = core.getName(next);
+    //     programms.push({
+    //       address: next,
+    //       name
+    //     });
+    //   }
+    //   next = core.next(next);
+    //   if (next == '0x0000000000000000000000000000000000000000') {
+    //     next = false;
+    //   }
+    // }
     resolve(programms);
   });
 }
@@ -41,11 +45,14 @@ export const getTokens = function(coreAddress) {
       next = false;
     }
     while (next) {
-      let name = core.getName(next);
-      tokens.push({
-        address: next,
-        name
-      });
+      const type = core.abiOf(next);
+      if (type === 'tokenAcl' || type === 'token-acl') {
+        let name = core.getName(next);
+        tokens.push({
+          address: next,
+          name
+        });
+      }
       next = core.next(next);
       if (next == '0x0000000000000000000000000000000000000000') {
         next = false;
@@ -58,7 +65,19 @@ export const getTokens = function(coreAddress) {
 export const getBalance = function(tokenAddress, complierAddress) {
   return new Promise((resolve, reject) => {
     const token = web3.eth.contract(abi.Token).at(tokenAddress);
-      token.balanceOf(complierAddress, (e, r) => {
+    token.balanceOf(complierAddress, (e, r) => {
+      if (e) {
+        reject(e);
+      }
+      resolve(r);
+    });
+  });
+}
+
+export const getTimestamp = function(tokenAddress) {
+  return new Promise((resolve, reject) => {
+    const token = web3.eth.contract(abi.Token).at(tokenAddress);
+    token.timestamp((e, r) => {
       if (e) {
         reject(e);
       }
@@ -74,6 +93,20 @@ export const getTransactionCount = function(address) {
         reject(e);
       }
       resolve(r);
+    });
+  });
+}
+
+export const isTransactionComplete = function(hash) {
+  return new Promise((resolve, reject) => {
+    web3.eth.getTransaction(hash, (e, tx) => {
+      if (e) {
+        reject(e);
+      }
+      if (tx && tx.blockNumber > 0) {
+        resolve(true);
+      }
+      resolve(false);
     });
   });
 }
